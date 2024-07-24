e92ebe81f280afbeb3f0839ce92e32bb0c1cc38d
enesozkurt <enes.ozkurt4@gmail.com>
2024-07-24 21:05:41 +0300

    mobile and left sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,26 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "@/node_modules/next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = { firstName: 'Enes', lastName: 'Özkurt' }
   return (
-    <main>
-       SIDEBAR
-       {children} 
+    <main className="flex h-screen w-full font-inter">
+       <Sidebar user={loggedIn} />
+       <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children} 
+       </div>
     </main>
   );
 }
